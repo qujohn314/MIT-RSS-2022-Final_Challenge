fdb05cdae80d9a158966658265dcf2f9a0d6b9ba
SpacePanda-12 <dfgoff22@gmail.com>
2022-04-27 19:57:22 +0000

    couple tweaks

--- road_detector/lane_detection.py
+++ road_detector/lane_detection.py
@@ -7,8 +7,9 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from final_challenge.msg import LaneLines
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import Point #geometry_msgs not in CMake file
+from geometry_msgs.msg import Point  # geometry_msgs not in CMake file
 from lane_detection.msg import LaneLines
+
 
 class LaneDetector():
     """
@@ -17,7 +18,7 @@ class LaneDetector():
     Publishes to: /relative_lane_lines (LaneLines) : the coordinates of the line in the image frame (units are pixels).
     """
     def __init__(self):
-    # toggle line follower vs cone parker
+        # toggle line follower vs cone parker
         self.LineFollower = False
 
         # Subscribe to ZED camera RGB frames
